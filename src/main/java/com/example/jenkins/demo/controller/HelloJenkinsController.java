607ffe0eaa42c7f6bc434df704da12d962e2d6df
blake <blake@ganguo.hk>
2019-02-26 14:19:47 +0800

    test: remove log

--- src/main/java/com/example/jenkins/demo/controller/HelloJenkinsController.java
+++ src/main/java/com/example/jenkins/demo/controller/HelloJenkinsController.java
@@ -1,6 +1,5 @@
 package com.example.jenkins.demo.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/hello")
-@Slf4j
 public class HelloJenkinsController {
 
     @GetMapping
     public String printHelloJenkins() {
 
-        log.info("HelloJenkinsController.printHelloJenkins =========== hello,Jenkins debugging at first line! =========== ");
-        log.info("HelloJenkinsController.printHelloJenkins =========== hello,Jenkins debugging at second line! =========== ");
-
-        return "hello,Jenkins 20190226131106";
+        return "hello,Jenkins :first time!";
     }
 
 }
